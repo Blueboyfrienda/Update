efd1882175cfc565ea31de78dee3c1d1d8ee8a2f
Blueboyfrienda <95651756+Blueboyfrienda@users.noreply.github.com>
2021-12-15 15:10:04 -0300

    Add files via upload

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -88,7 +88,7 @@ class MainMenuState extends MusicBeatState
 		for (i in 0...optionShit.length)
 		{
 			var offset:Float = 108 - (Math.max(optionShit.length, 4) - 4) * 80;
-			var menuItem:FlxSprite = new FlxSprite(0, 100 * (i * 140));
+			var menuItem:FlxSprite = new FlxSprite(0, 110 + (i * 100));
 			menuItem.frames = Paths.getSparrowAtlas('mainmenu/menu_' + optionShit[i]);
 			menuItem.animation.addByPrefix('idle', optionShit[i] + " basic", 24);
 			menuItem.animation.addByPrefix('selected', optionShit[i] + " white", 24);
@@ -241,7 +241,7 @@ class MainMenuState extends MusicBeatState
 
 		menuItems.forEach(function(spr:FlxSprite)
 		{
-			spr.screenCenter(X);
+			
 		});
 	}
 
